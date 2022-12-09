456408177450a026232b8c33743fc0c81ba75b64
naveen8801 <naveensharma10d@gmail.com>
2022-12-09 10:25:31 +0530

    refector

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,21 +1,37 @@
 import React from 'react';
 import Post from '../Posts/Post/Post';
 import useStyles from './styles';
-import {useSelector} from 'react-redux';
-import {Grid , CircularProgress } from '@material-ui/core';
+import { useSelector } from 'react-redux';
+import { Grid, CircularProgress } from '@material-ui/core';
 
-function Posts({setCurrentId}) {
-    const classes = useStyles();
-    const posts = useSelector(state => state.posts);
-    console.log(posts);
+function Posts({ setCurrentId }) {
+  const classes = useStyles();
+  const posts = useSelector((state) => state.posts);
+
   return (
-      !posts.length ? <CircularProgress /> : (
-        <Grid className={classes.mainContainer} container alignItems="stretch" spacing={3} >
-          {posts.map((post)=>(<Grid key={post._id} item xs={12} sm={6}>
-            <Post post={post} setCurrentId = {setCurrentId} />
-            </Grid>))}
-        </Grid>
-      )
+    <>
+      {posts.length === 0 ? (
+        <h2>No Posts</h2>
+      ) : (
+        <>
+          <Grid
+            className={classes.mainContainer}
+            container
+            alignItems="stretch"
+            spacing={3}
+          >
+            {posts.map((post) => (
+              <Grid key={post._id} item xs={12} sm={6}>
+                <Post post={post} setCurrentId={setCurrentId} />
+              </Grid>
+            ))}
+          </Grid>
+        </>
+      )}
+    </>
+    // !posts.length ? <CircularProgress /> : (
+
+    // )
   );
 }
 
